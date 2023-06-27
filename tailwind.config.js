359d9e92a3d323b521c51e50e417709e5a49779b
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-06-27 16:07:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'gulag-blue': '#e2edff',
+      },
+    },
+  },
+  plugins: [],
+}
+
