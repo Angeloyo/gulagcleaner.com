505e485c34c8dcfdc3e618b4e881762f0cee6639
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-06-29 12:18:14 +0200

    modo oscuro lenin cambia de color

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-// Icons
+// Icons and img
 const sunIcon = document.getElementById("sun");
 const moonIcon = document.getElementById("moon");
 const switchButton = document.getElementById("switchButton")
+const blackLenin = document.getElementById("black-lenin");
+const whiteLenin = document.getElementById("white-lenin");
 
 // Theme vars
 const userTheme = localStorage.getItem("theme");
@@ -13,6 +15,8 @@ const iconToggle = () => {
     // sunIcon.classList.toggle("hidden");
     moonIcon.classList.toggle("display-none");
     sunIcon.classList.toggle("display-none");
+    blackLenin.classList.toggle("display-none");
+    whiteLenin.classList.toggle("display-none");
 }
 
 // Initial Theme check
@@ -21,9 +25,13 @@ const themeCheck = () => {
         document.documentElement.classList.add("dark");
         // moonIcon.classList.add("hidden");
         moonIcon.classList.add("display-none");
+        blackLenin.classList.toggle("display-none");
+
         return;
     }
     sunIcon.classList.add("display-none");
+    whiteLenin.classList.toggle("display-none");
+
 }
 
 // Manual theme switch
